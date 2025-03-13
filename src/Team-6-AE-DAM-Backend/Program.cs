12a9274450559d32edcfb874b4284d5c11283648
mukund <mukunda05@gmail.com>
2025-03-12 23:47:56 -0700

    Adding to SQL database for projects

--- src/Team-6-AE-DAM-Backend/Program.cs
+++ src/Team-6-AE-DAM-Backend/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureSQL")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<AuthService>();
 
