24d4ce27ac51d6b336d3e6943d951817b4e42c7c
Mandy Deng <miaodan@students.cs.ubc.ca>
2025-03-21 14:38:11 -0700

    got rid of unnecessary access levels, reformated page

--- src/frontend/src/pages/admin/AdminProjectSecurity.js
+++ src/frontend/src/pages/admin/AdminProjectSecurity.js
@@ -23,8 +23,6 @@ const users = [
 {name: 'Michael Johnson', role: 'User', status: 'Inactive'}
 ];*/
 
-const metadata = ["Project Name", "Location", "Date", "Image Description", "Tags"];
-
 function PopupAccess(adminChecked, setAdminChecked, allChecked, setAllChecked, selectedChecked, setSelectedChecked, listUsers, setListUsers, fetchedUsersForAProject, setFetchedUsersForProject, originalUsersForProject, setOriginalUsersForProject) {
 /*const [adminChecked, setAdminChecked] = useState(project.accessLevel === 'Admins Only');
 const [allChecked, setAllChecked] = useState(project.accessLevel === 'Everyone');
@@ -146,7 +144,7 @@ flexDirection: 'column',
 justifyContent: 'flex-start',
 alignItems: 'left',
 width: '80%',
-height: '100%',
+height: '50%',
 margin: '20px auto',
 marginTop: '0',
 backgroundColor: '#f5f5f5',
@@ -182,24 +180,7 @@ overflow: 'auto'
 
         }</td>
     </tr>
-    {metadata.map((md) => (
-        <tr style={{height: '50px'}}>
-            <td style={{ fontSize: '12px', textAlign: 'left', borderBottom:'1px solid black'}} >{md}</td>
-            <td style={{ fontSize: '12px', width: '5%', textAlign: 'center', borderBottom:'1px solid black'}} >{
-                // reload if status input differs from original user.status
-                <Popover
-                    content={
-                        <Radio.Group style={{ display: 'flex', flexDirection: 'column', marginTop: "0px" }}>
-                            <Radio value="admin"> <p style={{fontSize:'12px', margin: '0px'}}>Admin Only</p> </Radio>
-                            <Radio value="everyone"> <p style={{fontSize:'12px', margin: '0px'}}>Everyone</p> </Radio>
-                        </Radio.Group>}
-                    trigger="click"
-                >
-                    <Button color="default" variant="text" size={"default"} icon={<EditOutlined />}/>
-                </Popover>
-            }</td>
-        </tr>
-    ))}
+    
 </table>
 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', margin: '20px auto', marginBottom: '0'}}>
     <Button color="default" variant="text" size={"default"} icon={<CloseOutlined/>}
@@ -332,6 +313,28 @@ sx={{
 <Title level={1}>Project Security</Title>
 </Box>
 
+<Box
+sx={{
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'left',
+  padding: '20px',
+  paddingBottom: 0,
+  margin: '10px auto',
+  marginBottom: '0',
+}}
+>
+  <Input
+    placeholder="Search for a project.."
+    prefix={<SearchOutlined />}
+    value={searchQuery}
+    onChange={(e) => setSearchQuery(e.target.value)}
+    style={{ width: '300px' }}
+    disabled={loading}
+  />
+</Box>
+
 
 <Box
 sx={{
@@ -358,18 +361,10 @@ sx={{
         alignItems: 'left',
         width: '50%',
         margin: '20px auto',
+        marginTop: '0',
         borderRadius: '10px',
     }}
 >
-
-    <Input
-        placeholder="Search for a project.."
-        prefix={<SearchOutlined />}
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        style={{ width: '300px' }}
-        disabled={loading}
-    />
 
     <Box
         sx={{
@@ -455,8 +450,9 @@ sx={{
         alignItems: 'center',
         width: '40%',
         padding: '20px',
+        margin: '20px auto',
+        marginTop: '0',
         paddingBottom: '10',
-        paddingTop: '0'
     }}
 >
     {isPopupFormOpen && popupForm(project, setPopupFormOpen, adminChecked, setAdminChecked, allChecked, setAllChecked, selectedChecked, setSelectedChecked, listUsers, setListUsers, fetchedUsersForAProject, setFetchedUsersForProject, originalUsersForProject, setOriginalUsersForProject)}
