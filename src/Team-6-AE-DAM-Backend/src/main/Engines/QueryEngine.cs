edff1a7dca838bff4d7473119f6b693f5ceeb540
pranjali <pranjalidas2201@gmail.com>
2025-03-28 11:24:58 -0700

    query Engine - for project fields

--- src/Team-6-AE-DAM-Backend/src/main/Engines/QueryEngine.cs
+++ src/Team-6-AE-DAM-Backend/src/main/Engines/QueryEngine.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+
 namespace DAMBackend.Engine
 {
-    public class QueryEngine
-    {
+//    public string Name { get; set; }
+    public string Status { get; set; }
+    public string? Location { get; set; }
+     public DateTime StartDate { get; set; }
 
+            public QueryEngine(string status, string location, DateTime date)
+            {
+                Status = status;
+                Location = location;
+                Date = date;
+            }
     }
