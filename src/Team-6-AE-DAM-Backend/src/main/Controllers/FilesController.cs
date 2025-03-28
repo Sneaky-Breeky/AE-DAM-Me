bb249c62c683526f4adafe765357c2f5bb272b9a
pranjali <pranjalidas2201@gmail.com>
2025-03-28 15:31:53 -0700

    Add Exif functions

--- src/Team-6-AE-DAM-Backend/src/main/Controllers/FilesController.cs
+++ src/Team-6-AE-DAM-Backend/src/main/Controllers/FilesController.cs
@@ -219,6 +219,21 @@ namespace DAMBackend.Controllers
                 };
                 //call exif function and update fileModel object before saving
                 //addExifData(fileModel)
+                // ADDING EXIF
+                using (var stream = imageFile.OpenReadStream())
+                            using (var image = Image.Load(stream))
+                            {
+                                fileModel.PixelWidth = image.Width;
+                                fileModel.PixelHeight = image.Height;
+                                fileModel.Palette = true;
+
+                                var exifProfile = image.Metadata.ExifProfile;
+                                if (exifProfile != null)
+                                {
+                                    ExtractExifMetadata(exifProfile, fileModel);
+                                }
+                            }
+                //
                 _context.Files.Add(fileModel);
                 savedFiles.Add(fileModel);
             }
@@ -272,5 +287,82 @@ namespace DAMBackend.Controllers
         {
             return _context.Files.Any(e => e.Id == id);
         }
+        private void ExtractExifMetadata(ImageSharpExif.ExifProfile exifProfile, FileModel fileModel)
+                {
+                    object? latRef = null;
+                    object? lonRef = null;
+
+                    foreach (var tag in exifProfile.Values)
+                    {
+                        if (tag.Tag == ImageSharpExifTag.GPSLatitudeRef)
+                        {
+                            latRef = tag.GetValue();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.GPSLongitudeRef)
+                        {
+                            lonRef = tag.GetValue();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.GPSLatitude)
+                        {
+                            fileModel.GPSLat = ConvertDMSToDecimal(tag.GetValue(), latRef?.ToString());
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.GPSLongitude)
+                        {
+                            fileModel.GPSLon = ConvertDMSToDecimal(tag.GetValue(), lonRef?.ToString());
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.GPSAltitude && tag.GetValue() is SixLabors.ImageSharp.Rational altitudeRational)
+                        {
+                            fileModel.GPSAlt = (decimal)altitudeRational.ToDouble();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.Make)
+                        {
+                            fileModel.Make = tag.GetValue()?.ToString();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.Model)
+                        {
+                            fileModel.Model = tag.GetValue()?.ToString();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.Copyright)
+                        {
+                            fileModel.Copyright = tag.GetValue()?.ToString();
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.FocalLength)
+                        {
+                            if (tag.GetValue() is SixLabors.ImageSharp.Rational rational)
+                            {
+                                fileModel.FocalLength = (int)rational.ToDouble();
+                            }
+                        }
+                        else if (tag.Tag == ImageSharpExifTag.FNumber)
+                        {
+                            if (tag.GetValue() is SixLabors.ImageSharp.Rational rational)
+                            {
+                                fileModel.Aperture = (float)rational.ToDouble();
+                            }
+                        }
+                    }
+                }
+
+                private decimal? ConvertDMSToDecimal(object dmsValue, string? reference)
+                {
+                    if (dmsValue is SixLabors.ImageSharp.Rational[] dmsArray && dmsArray.Length == 3)
+                    {
+                        decimal degrees = (decimal)dmsArray[0].ToDouble();
+                        decimal minutes = (decimal)dmsArray[1].ToDouble();
+                        decimal seconds = (decimal)dmsArray[2].ToDouble();
+
+                        decimal decimalDegrees = degrees + (minutes / 60) + (seconds / 3600);
+
+                        if (reference == "S" || reference == "W")
+                        {
+                            decimalDegrees = -decimalDegrees;
+                        }
+
+                        return decimalDegrees;
+                    }
+
+                    return null;
+                }
+            }
     }
 }
