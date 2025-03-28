0daf658ccf2f07617ab5150ff1767feb38b136c4
mukund <mukunda05@gmail.com>
2025-03-28 11:15:27 -0700

    Added fields to file

---
+++ src/Team-6-AE-DAM-Backend/Migrations/20250328181350_AddFieldsToFile.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAMBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsToFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LogImage",
+                table: "LogImage");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Files",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Resolution",
+                table: "Files",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LogImage",
+                table: "LogImage",
+                column: "LogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_LogImage",
+                table: "LogImage");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Files");
+
+            migrationBuilder.DropColumn(
+                name: "Resolution",
+                table: "Files");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_LogImage",
+                table: "LogImage",
+                columns: new[] { "LogId", "UserId" });
+        }
+    }
+}
