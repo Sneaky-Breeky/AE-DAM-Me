aa958f000e3edb0bb948c6a0c907fe6c0067dc3f
Pranjali Das <109291107+pranjali-22@users.noreply.github.com>
2025-04-06 20:56:55 -0700

    Update FilesController.cs

--- src/Team-6-AE-DAM-Backend/src/main/Controllers/FilesController.cs
+++ src/Team-6-AE-DAM-Backend/src/main/Controllers/FilesController.cs
@@ -43,7 +43,6 @@ namespace DAMBackend.Controllers
                 public int? FocalLength { get; set; }
                 public float? Aperture { get; set; }
                 public string? Copyright { get; set; }
-        // Exif data
     }
 
     [Route("api/[controller]")]
@@ -177,16 +176,10 @@ namespace DAMBackend.Controllers
                 using var streamthumbnail = thumbnail.OpenReadStream();
                 string fileUrlThumbnail = await _azureBlobService.UploadThumbnailAsync(thumbnail, fileNameThumbnail);
 
-                UpladedFile uploadedFile = new UpladedFile
-                {
-                    ThumbnailPath = fileUrlThumbnail,
-                    OriginalPath = fileUrlOriginal,
+                var uploadedFile = ProcessImageToExif(file);
+                uploadedFile.ThumbnailPath = fileNameThumbnail;
+                uploadedFile.OriginalPath = fileUrlOriginal;
 
-
-                };
-
-                // filesLinks.Add(new {thumbnail = fileUrlThumbnail, origin = fileUrlOriginal});
-                // Console.WriteLine($"bruh: {fileUrlOriginal} {fileUrlThumbnail}");
                 filesLinks.Add(uploadedFile);
             }
 
@@ -617,9 +610,122 @@ namespace DAMBackend.Controllers
           }
       }
 
+public UpladedFile ProcessImageToExif(IFormFile imageFile)
+                         {
+                             string originalPath = "not for now";
+                             string viewPath = "not for now";
+                             string thumbnailPath = "not for now";
 
+                             var fileModel = new UpladedFile
+                             {
+//                                 Name = Path.GetFileNameWithoutExtension(imageFile.FileName),
+//                                 Extension = Path.GetExtension(imageFile.FileName),
+                                 ThumbnailPath = thumbnailPath,
+//                                 ViewPath = viewPath,
+                                 OriginalPath = originalPath,
+                                 PixelWidth = 0,
+                                 PixelHeight = 0,
+
+                             };
+
+                             using (var stream = imageFile.OpenReadStream())
+                             using (var image = SixLabors.ImageSharp.Image.Load(stream))
+                             {
+                                 fileModel.PixelWidth = image.Width;
+                                 fileModel.PixelHeight = image.Height;
+//                                 fileModel.Palette = true;
+
+                                 var exifProfile = image.Metadata.ExifProfile;
+                                 if (exifProfile != null)
+                                 {
+                                     ExtractExifMetadata(exifProfile, fileModel);
+                                 }
+                             }
+
+                             return fileModel;
+                         }
+
+
+      private UpladedFile ExtractExifMetadata(ImageSharpExif.ExifProfile exifProfile, UpladedFile fileModel)
+              {
+                  object? latRef = null;
+                  object? lonRef = null;
+
+                  foreach (var tag in exifProfile.Values)
+                  {
+                      if (tag.Tag == ImageSharpExifTag.GPSLatitudeRef)
+                      {
+                          latRef = tag.GetValue();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.GPSLongitudeRef)
+                      {
+                          lonRef = tag.GetValue();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.GPSLatitude)
+                      {
+                          fileModel.GPSLat = ConvertDMSToDecimal(tag.GetValue(), latRef?.ToString());
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.GPSLongitude)
+                      {
+                          fileModel.GPSLon = ConvertDMSToDecimal(tag.GetValue(), lonRef?.ToString());
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.GPSAltitude &&
+                               tag.GetValue() is SixLabors.ImageSharp.Rational altitudeRational)
+                      {
+                          fileModel.GPSAlt = (decimal)altitudeRational.ToDouble();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.Make)
+                      {
+                          fileModel.Make = tag.GetValue()?.ToString();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.Model)
+                      {
+                          fileModel.Model = tag.GetValue()?.ToString();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.Copyright)
+                      {
+                          fileModel.Copyright = tag.GetValue()?.ToString();
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.FocalLength)
+                      {
+                          if (tag.GetValue() is SixLabors.ImageSharp.Rational rational)
+                          {
+                              fileModel.FocalLength = (int)rational.ToDouble();
+                          }
+                      }
+                      else if (tag.Tag == ImageSharpExifTag.FNumber)
+                      {
+                          if (tag.GetValue() is SixLabors.ImageSharp.Rational rational)
+                          {
+                              fileModel.Aperture = (float)rational.ToDouble();
+                          }
+                      }
+                  }
+                  return fileModel;
+              }
+               private decimal? ConvertDMSToDecimal(object dmsValue, string? reference)
+                      {
+                          if (dmsValue is SixLabors.ImageSharp.Rational[] dmsArray && dmsArray.Length == 3)
+                          {
+                              decimal degrees = (decimal)dmsArray[0].ToDouble();
+                              decimal minutes = (decimal)dmsArray[1].ToDouble();
+                              decimal seconds = (decimal)dmsArray[2].ToDouble();
+
+                              decimal decimalDegrees = degrees + (minutes / 60) + (seconds / 3600);
+
+                              if (reference == "S" || reference == "W")
+                              {
+                                  decimalDegrees = -decimalDegrees;
+                              }
+
+                              return decimalDegrees;
+                          }
+
+                          return null;
+                      }
 
 }
+
 }
 //}
 
